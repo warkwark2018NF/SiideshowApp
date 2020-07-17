fe48c6a5524e7d513db72a2219b00f8e21ec8fdd
warkwark2018NF <fusama_m@me.com>
2020-07-17 10:08:52 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by FUSAMAMASAKI on 2020/07/17.
+//  Copyright © 2020 FUSAMAMASAKI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
