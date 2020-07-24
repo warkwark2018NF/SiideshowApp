4f911716a25f7d88e45699851ef4b55901a61239
warkwark2018NF <fusama_m@me.com>
2020-07-24 20:34:49 +0900

    再生・停止釦でタイトル表示を切り替えた。自動送りでタップ有効・無効を明確化した。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -19,15 +19,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
         smallView.image = UIImage(named:"gu")
     }
-    @IBAction func small(_ sender: Any) {
-        self.timer.invalidate()                //２秒自動画像送りのタイマー停止。初回提出後に追加した!
-        self.timer = nil                       //２秒自動画像送りのタイマー停止。初回提出後に追加した!
-    }
+    
+    @IBAction func small(_ sender: Any) {               //第一画面の画像釦(small)
         
-    @IBAction func aheadButton(_ sender: Any) {
+        if self.timer == nil {
+        return                       //タイマー停止中なので、単に画面遷移する
+        
+      } else {
+        if self.timer != nil {
+            
+        self.timer.invalidate()      //タイマー稼働中なので、画面遷移前にタイマー停止する
+        self.timer = nil
+    
+        }
+      }
+    }
+    
+    
+    
+    
+        
+    @IBAction func aheadButton(_ sender: Any) {     //進む釦の機能プログラム
     
      if self.timer == nil {
         
@@ -47,8 +63,9 @@ class ViewController: UIViewController {
     }
 
 
-    @IBAction func backButton(_ sender: Any) {
-     if self.timer == nil {
+    @IBAction func backButton(_ sender: Any) {      //戻る釦の機能プログラム
+        
+        if self.timer == nil {
         self.answerNumber -= 1
         if self.answerNumber < 0 {
         self.answerNumber = self.answerNumber + 3
@@ -71,45 +88,57 @@ class ViewController: UIViewController {
     var timer:Timer!
     var timer_sec:Int = 0
     var update:String = "gu"
-    @objc func updateTimer(_ timer:Timer){          // ？疑問だった？******
+    @objc func updateTimer(_ timer:Timer){          // *****？疑問だった？******
         self.timer_sec += 1
         self.answerNumber += 1
         self.answerNumber %= 3
         
         if self.answerNumber == 0 {
             smallView.image = UIImage(named:"gu")
-            print("2秒自動画像= gu")
+            print("2秒自動画像 = gu")
            } else if self.answerNumber == 1 {
             smallView.image = UIImage(named:"choki")
-            print("2秒自動画像= choki")
+            print("2秒自動画像 = choki")
            } else if self.answerNumber == 2 {
             smallView.image = UIImage(named:"pa")
-            print("2秒自動画像= pa")
+            print("2秒自動画像 = pa")
         }
-                                                    // ？疑問だった？******
-        
-    }
+    }                                               // *****？疑問だった？******
+   
     
     var count:Int = 0           //再生・停止ボタンのセット/リセット制御変数の初期化
-    @IBAction func setReset(_ sender: Any) {
-        ahead.isEnabled = false                  //進む釦の無効。初回提出後に追加した!
-        back.isEnabled = false                   //戻る釦の無効。初回提出後に追加した!
-        count %= 2      //再生・停止ボタンのセット/リセット制御変数の仕分け
-        if count == 0 {
+   
+    @IBOutlet weak var setReset: UIButton!          //再生・停止ボタン(setReset)
+    
+    @IBAction func setReset(_ sender: UIButton) {
+    
+        //"isEnabled = false"移動前の位置：ココ
+        
+        count %= 2                  //再生・停止ボタンのセット/リセット制御変数で仕分け
+        if count == 0 {             //再生状態である
+          sender.setTitle("停止", for: .normal)
+            
           if self.timer == nil {
+            ahead.isEnabled = false                    //進む釦の無効。初回提出後に追加した!
+            back.isEnabled = false                     //戻る釦の無効。初回提出後に追加した!
+            
             self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
             count += 1
             }
          }
-         else {
+         else {                     //停止状態である
+            
+          sender.setTitle("再生", for: .normal)
+            
           if self.timer != nil {
+            
                 self.timer.invalidate()
                 self.timer = nil
+                ahead.isEnabled = true                       //進む釦の有効。初回提出後に追加した!
+                back.isEnabled = true                        //戻る釦の有効。初回提出後に追加した!
                 count += 1
-          }
+            }
         }
-        ahead.isEnabled = true                       //進む釦の有効。初回提出後に追加した!
-        back.isEnabled = true                        //戻る釦の有効。初回提出後に追加した!
         print("count[再生:1,停止:2] =\(count)")
         print("timer_sec =\(timer_sec)")
     }
@@ -124,12 +153,25 @@ class ViewController: UIViewController {
     
     @IBAction func unwind(_ segue:UIStoryboardSegue){
         
-                                                //２秒自動画像送りのタイマー有効。初回提出後に追加した!
-        if self.timer == nil {
-           self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
-           }                                    //２秒自動画像送りのタイマー有効。初回提出後に追加した!
+        if count == 1 {             //画像遷移前はタイマー稼働中だった
+            
+            ahead.isEnabled = false                    //進む釦の無効。初回提出後に追加した!
+            back.isEnabled = false                     //戻る釦の無効。初回提出後に追加した!
+            
+            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+
         
+         } else {                   //画像遷移前はタイマー停止中だった
+            
+
+                ahead.isEnabled = true                       //進む釦の有効。初回提出後に追加した!
+                back.isEnabled = true                        //戻る釦の有効。初回提出後に追加した!
+            
+    
         
+        }
+        print("count2 [再生:1,停止:2] =\(count)")
+        print("timer_sec2 =\(timer_sec)")
     }
     
 }
